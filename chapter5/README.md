dd8fc5ba842bf3c535a07fffd63704acaf6f6ad4
hm4725 <4725hmc@gmail.com>
2022-10-18 19:35:34 +0900

    Chapter5: consensus

---
+++ chapter5/README.md
@@ -0,0 +1,49 @@
+# Evaluation
+## Environment
+### HW
+```
+Processor    : Apple M1 chip
+Configuration: 8-core CPU with 4 perform­ance cores and 4 efficiency cores
+Main Memory  : 8GB unified memory
+Storage      : APPLE SSD AP0256Q 256GB
+```
+### SW
+```
+OS: macOS Monterey v12.6
+clang: Apple clang version 13.1.6 (clang-1316.0.21.2.5)
+```
+## How to build
+```sh
+make
+```
+## How to execute
+```sh
+./bin/{Program} < workload.txt
+```
+Change `{Program}` for each code.
+
+## Results
+### flag
+```
+[61632 / 61632]
+Fails: 75
+```
+> Consensus number: 1
+### queue
+```
+[61632 / 61632]
+Fails: 246
+```
+> Consensus number: 1
+### multi_assign
+```
+[61632 / 61632]
+Fails: 0
+```
+> Consensus number: 2
+### atomic_flag (get_and_fetch)
+```
+[61632 / 61632]
+Fails: 0
+```
+> Consensus number: 2
---
+++ chapter5/src/queue.cpp
@@ -0,0 +1,78 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <queue>
+#include <algorithm>
+
+#define DEFAULT -1
+#define TOTAL_LINES 61632
+#define REDBALL 1
+#define BLACKBALL 2
+
+// Shared
+volatile int32_t proposed[2];
+std::queue<int> q;
+// Thread locals
+int32_t inputs[2];
+
+void propose(int32_t value, int64_t id) {
+  proposed[id] = value;
+}
+
+int32_t decide(int32_t value, int64_t id) {
+  propose(value, id);
+  int ball = q.front();
+  q.pop();
+  if(ball == REDBALL) {
+    return value;
+  } else {
+    return proposed[1-id];
+  }
+}
+
+void* worker(void* args) {
+  int64_t id = (int64_t)args;
+  int32_t value = inputs[id];
+  int32_t result = decide(value, id);
+  return (void*)(int64_t)result;
+}
+
+int main() {
+  pthread_t threads[2];
+  int32_t decided[2];
+  int nfails = 0;
+  int counter = 0;
+  while(1) {
+    // init
+    inputs[0] = DEFAULT;
+    inputs[1] = DEFAULT;
+    proposed[0] = DEFAULT;
+    proposed[1] = DEFAULT;
+    std::queue<int> empty;
+    std::swap(q, empty);
+    q.push(REDBALL);
+    q.push(BLACKBALL);
+
+    // input values
+    scanf("%d", &inputs[0]);
+    if(inputs[0] < 0) {
+      break;
+    }
+    scanf("%d", &inputs[1]);
+
+    // thread
+    pthread_create(&threads[0], NULL, worker, (void*)0);
+    pthread_create(&threads[1], NULL, worker, (void*)1);
+
+    pthread_join(threads[0], (void**)&decided[0]);
+    pthread_join(threads[1], (void**)&decided[1]);
+
+    if(decided[0] != decided[1]) {
+      nfails += 1;
+    }
+    counter += 1;
+    printf("\x1B[2J\x1B[H");
+    printf("[%d / %d]\n", counter, TOTAL_LINES);
+    printf("Fails: %d\n", nfails);
+  }
+  return 0;
+}
